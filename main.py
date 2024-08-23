c03aa506b22c294bcb8d898fddf101bcd70d258f
Jean-Léon Henry <92361407+JeanLeonHenry@users.noreply.github.com>
2024-08-23 20:00:07 +0200

    feat: ship + asteroids movement

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 
-from constants import *
+from constants import PLAYER_RADIUS, SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -9,12 +12,32 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
         screen.fill("#000000")
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1_000
 
 
 if __name__ == "__main__":
